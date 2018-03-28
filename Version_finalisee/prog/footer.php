19e513ccf98e9ee5cf79d4a41010317017df2238
Tabardelli <remi.tabardel@free.fr>
2018-03-28 16:36:36 +0200

    la merde de menage

--- Version_finalisee/prog/footer.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/pgwslideshow.min.js"></script>
-<script type="text/javascript" src="js/carousel_profil.js"></script>
-<script type="text/javascript" src="js/profil.js"></script>
-</body>
-</html>
